248b50f37d7d2a4d623ecc9edf460507f8b2dcbb
fuchsfoxi <165415707+fuchsfoxi@users.noreply.github.com>
2026-04-23 16:25:37 -0500

    dada

--- app/index.php
+++ app/index.php
@@ -9,7 +9,10 @@ echo phpinfo();
        <body>
        <?php //abrimos php
        //echo phpinfo();
-       //cerrmos php ¿>
+       $nombre = "Juan";
+       $edad = 16;
+       //cerrmos php 
+       // ¿>
 
        <P> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis maiores perferendis recusandae tempora? Tempore eligendi ipsa ut. Libero id magni repellat illum placeat voluptates omnis velit corporis sunt numquam quos eveniet quasi nobis recusandae modi reiciendis perspiciatis minus iure tenetur, earum fugit itaque et? Dolorum velit tenetur voluptas ipsum quo?</P>
        
