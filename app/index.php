a2719931f006c1c4f464d8c3ee47fddc2ee10828
fuchsfoxi <165415707+fuchsfoxi@users.noreply.github.com>
2026-04-23 16:19:24 -0500

    DADA

---
+++ app/index.php
@@ -0,0 +1,16 @@
+<?php // abrimos php
+
+echo phpinfo();
+
+<html>
+       <head>
+              <title> EMPRESA </title>
+       </head>
+       <body>
+       <?php //abrimos php
+       echo phpinfo();
+       //cerrmos php ¿>
+
+       <P> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis maiores perferendis recusandae tempora? Tempore eligendi ipsa ut. Libero id magni repellat illum placeat voluptates omnis velit corporis sunt numquam quos eveniet quasi nobis recusandae modi reiciendis perspiciatis minus iure tenetur, earum fugit itaque et? Dolorum velit tenetur voluptas ipsum quo?</P>
+       <body>
+</html>
